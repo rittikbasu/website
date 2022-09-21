48a2cab230f454ed273b18e8a54c7e1e7bb618c7
rittikbasu <irittik@gmail.com>
2022-09-22 04:00:35 +0530

    fix: remove redundant useEffect dependency

--- src/lib/formatDate.js
+++ src/lib/formatDate.js
@@ -1,27 +1,16 @@
 import { useState, useEffect } from 'react'
 
-// export function formatDate(dateString) {
-//   return new Date(`${dateString}T00:00:00Z`).toLocaleDateString('en-US', {
-//     day: 'numeric',
-//     month: 'long',
-//     year: 'numeric',
-//     timeZone: 'UTC',
-//   })
-// }
-
 export const formatDate = (date) => {
   const [formattedDate, setFormattedDate] = useState(null)
 
-  useEffect(
-    () =>
-      setFormattedDate(
-        new Date(date).toLocaleDateString('en-US', {
-          month: 'short',
-          day: '2-digit',
-          year: 'numeric',
-        })
-      ),
-    []
+  useEffect(() =>
+    setFormattedDate(
+      new Date(date).toLocaleDateString('en-US', {
+        month: 'short',
+        day: '2-digit',
+        year: 'numeric',
+      })
+    )
   )
 
   return formattedDate
