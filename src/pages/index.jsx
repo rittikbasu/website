174c83462240acfc0403f9db1e978f6732cfb566
rittikbasu <irittik@gmail.com>
2022-09-22 01:38:13 +0530

    update: icons + content

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,8 +6,8 @@ import clsx from 'clsx'
 import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
-import { BsTwitter, BsGithub, BsLinkedin } from 'react-icons/bs'
-import { FaDiscord } from 'react-icons/fa'
+import { BsTwitter, BsGithub } from 'react-icons/bs'
+import { FaDiscord, FaLinkedinIn } from 'react-icons/fa'
 // import { generateRssFeed } from '@/lib/generateRssFeed'
 import { formatDate } from '@/lib/formatDate'
 
@@ -26,10 +26,15 @@ function Article({ article }) {
   )
 }
 
-function SocialLink({ icon: Icon, ...props }) {
+function SocialLink({ className, icon: Icon, ...props }) {
   return (
     <Link className="group -m-1 p-1" {...props}>
-      <Icon className="h-5 w-5 fill-zinc-500 transition group-hover:fill-indigo-600 dark:fill-zinc-400 dark:group-hover:fill-indigo-300" />
+      <Icon
+        className={clsx(
+          className,
+          'h-5 w-5 fill-zinc-500 transition group-hover:fill-indigo-600 dark:fill-zinc-400 dark:group-hover:fill-indigo-500'
+        )}
+      />
     </Link>
   )
 }
@@ -39,21 +44,21 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Rittik Basu - Web Developer, founder, and amateur astronaut
+          Rittik Basu - Web Developer, founder, and amateur music producer
         </title>
         <meta
           name="description"
-          content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="I’m Rittik, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
         />
       </Head>
       <Container className="flex h-screen items-center justify-center">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Web developer, founder, and amateur astronaut.
+            Web developer, founder, and amateur music producer.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
+            I’m Rittik Basu, a computer engineer and technology enthusiast based
+            in India. I’m the founder and CEO of Planetaria, where we develop
             technologies that empower regular people to explore space on their
             own terms.
           </p>
@@ -67,6 +72,7 @@ export default function Home({ articles }) {
               href="https://instagram.com"
               aria-label="Follow on Instagram"
               icon={FaDiscord}
+              className="h-[1.35rem] w-[1.35rem]"
             />
             <SocialLink
               href="https://github.com"
@@ -76,7 +82,7 @@ export default function Home({ articles }) {
             <SocialLink
               href="https://linkedin.com"
               aria-label="Follow on LinkedIn"
-              icon={BsLinkedin}
+              icon={FaLinkedinIn}
             />
           </div>
         </div>
