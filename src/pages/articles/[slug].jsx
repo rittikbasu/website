3236d4952f154b15c1d2725721e2b45d4ac869fa
rittikbasu <irittik@gmail.com>
2022-09-23 05:21:41 +0530

    feat: replace link w router to go back

--- src/pages/articles/[slug].jsx
+++ src/pages/articles/[slug].jsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import Head from 'next/head'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import { getDatabase, getPage, getBlocks } from '@/lib/notion'
 
@@ -11,13 +11,14 @@ import { FormatDate } from '@/components/FormatDate'
 
 import { BsArrowLeft } from 'react-icons/bs'
 
-export default function Post({ article, blocks }) {
+export default function Post({ article, blocks, previousPathname }) {
   if (!article || !blocks) {
     return <div />
   }
   const date = FormatDate(article.created_time)
   const articleTitle = article.properties.name.title
   const articleDescription = article.properties.description.rich_text
+  let router = useRouter()
   return (
     <div>
       <Head>
@@ -28,15 +29,16 @@ export default function Post({ article, blocks }) {
       <Container className="mt-16 lg:mt-32">
         <div className="xl:relative">
           <div className="mx-auto max-w-2xl">
-            <Link href="/articles">
+            {previousPathname && (
               <button
                 type="button"
+                onClick={() => router.back()}
                 aria-label="Go back to articles"
                 className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:mb-0 lg:-mt-2 xl:-top-1.5 xl:left-0 xl:mt-0"
               >
                 <BsArrowLeft className="h-4 w-4 text-zinc-500 transition group-hover:text-zinc-700 dark:text-zinc-400 dark:group-hover:text-zinc-300" />
               </button>
-            </Link>
+            )}
             <article>
               <header className="flex flex-col">
                 <h1 className="my-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
