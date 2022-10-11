be33e5a81e1883f2a48701cb12793e914830c18f
rittikbasu <irittik@gmail.com>
2022-10-11 05:31:00 +0530

    feat: update structure + content

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -47,43 +47,36 @@ export default function Home({ articles }) {
       <NextSeo canonical={`${baseUrl}`} />
       <Container className="flex h-screen items-center justify-center">
         <div className="max-w-2xl">
+          <div className="pl-1 text-sm font-bold tracking-widest text-zinc-600 dark:text-zinc-400 md:text-base">
+            Hi, my name is
+          </div>
           <h1
-            className="animate-gradient bg-gradient-to-r from-purple-500 via-indigo-500 to-pink-500 bg-clip-text text-4xl  font-bold 
+            className="py-4 text-4xl font-bold tracking-wide  text-zinc-800 
+            dark:text-zinc-100 sm:text-5xl"
+          >
+            Rittik Basu.
+          </h1>
+          <h1
+            className="animate-gradient bg-gradient-to-r from-purple-500 via-indigo-500 to-pink-500 bg-clip-text text-[1.6rem]  font-bold 
             tracking-wide text-transparent dark:from-purple-400
             dark:via-indigo-400 dark:to-pink-400 sm:text-5xl"
           >
-            Web Developer, <br className="md:hidden" /> Designer & Amateur Music
-            Composer
+            I build things for the web.
           </h1>
-          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Rittik Basu, a computer engineer and technology enthusiast based
-            in India. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+          <p className="my-6 pl-1 text-base text-zinc-600 dark:text-zinc-400">
+            I&#39;m a computer engineer specializing in building & designing
+            scalable frontend products with great user experiences. Currently
+            I&#39;m working with Next.js & Tailwind and I ocassionally dabble in
+            datascience & blockchain technology.
           </p>
-          <div className="mt-6 flex gap-6">
-            <SocialLink
-              href="https://twitter.com"
-              aria-label="Follow on Twitter"
-              icon={BsTwitter}
-            />
-            <SocialLink
-              href="https://instagram.com"
-              aria-label="Follow on Instagram"
-              icon={FaDiscord}
-              className="h-[1.35rem] w-[1.35rem]"
-            />
-            <SocialLink
-              href="https://github.com"
-              aria-label="Follow on GitHub"
-              icon={BsGithub}
-            />
-            <SocialLink
-              href="https://linkedin.com"
-              aria-label="Follow on LinkedIn"
-              icon={FaLinkedinIn}
-            />
-          </div>
+
+          <Button
+            href="/about"
+            variant="primary"
+            className="group ml-1 h-8 tracking-widest outline outline-2 outline-indigo-300"
+          >
+            Learn More
+          </Button>
         </div>
       </Container>
     </>
