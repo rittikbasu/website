62c7382c0527f6a6be5c232a4b05c77109ac367d
rittikbasu <irittik@gmail.com>
2022-10-15 01:30:25 +0530

    feat: add gradient to title

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -6,7 +6,6 @@ import clsx from 'clsx'
 
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import { getDatabase } from '@/lib/notion'
 import { baseUrl } from '../seo.config'
 import data from '@/data/projects.js'
 
@@ -96,7 +95,8 @@ export default function ProjectsIndex({ projects }) {
         }}
       />
       <SimpleLayout
-        title="Things I’ve made trying to put my dent in the universe."
+        preTitle="Things I've made"
+        title="trying to put my dent in the universe."
         intro="I’ve worked on tons of little projects over the past couple of years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
       >
         <ul
