75b73be24158730c04164639a907f74d28b3d975
rittikbasu <irittik@gmail.com>
2022-10-17 02:48:12 +0530

    feat: change read article color

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -45,7 +45,7 @@ Card.Cta = function CardCta({ children }) {
   return (
     <div
       aria-hidden="true"
-      className="relative z-10 mt-4 flex items-center text-sm font-medium text-indigo-500 dark:text-indigo-400"
+      className="dark:text-indigo-500/7 relative z-10 mt-4 flex items-center text-sm font-medium text-indigo-500 dark:text-indigo-500/80"
     >
       {children}
       <BiChevronRight className="ml-1 h-4 w-4 stroke-current" />
