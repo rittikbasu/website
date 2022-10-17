10536dabc5b46b249fd8c297be14186392ef68c4
rittikbasu <irittik@gmail.com>
2022-10-18 03:18:32 +0530

    fix: remove h-screen content overlap with footer

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -41,7 +41,7 @@ function Article({ article }) {
 
 export default function ArticlesIndex({ articles }) {
   return (
-    <div className="h-full">
+    <>
       <NextSeo
         title="Blog"
         description="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
@@ -64,7 +64,7 @@ export default function ArticlesIndex({ articles }) {
           </div>
         </div>
       </SimpleLayout>
-    </div>
+    </>
   )
 }
 
