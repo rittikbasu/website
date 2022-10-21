3c23bbfd7b54d8d5ebf6d08000209fb198462395
rittikbasu <irittik@gmail.com>
2022-10-22 04:02:41 +0530

    feat: add font to local

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -51,8 +51,16 @@ export default function Document() {
           href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`}
         /> */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
-          rel="stylesheet"
+          rel="preload"
+          href="/fonts/Ubuntu-Bold.ttf"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Ubuntu-Regular.ttf"
+          as="font"
+          crossOrigin="anonymous"
         />
       </Head>
       <body className="flex h-full flex-col bg-zinc-50 dark:bg-black">
