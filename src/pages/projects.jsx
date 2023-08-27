2701f0b973067bb4ca91e26f84686a957b2db1ac
rittikbasu <irittik@gmail.com>
2023-08-28 01:10:34 +0530

    feat: update content + optimize images

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -36,7 +36,6 @@ function Project({ project, index }) {
           width="500"
           placeholder="blur"
           onLoadingComplete={() => setLoading(false)}
-          unoptimized
         />
       </div>
       <h2 className="z-10 mt-6 font-heading text-xl tracking-wider text-zinc-800 dark:text-zinc-100">
@@ -106,7 +105,7 @@ export default function ProjectsIndex({ projects }) {
       <SimpleLayout
         preTitle="Things I've made"
         title="trying to put my dent in the universe."
-        intro="I've worked on tons of little projects over the past couple of years but these are the ones that I'm most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+        intro="I've worked on tons of little projects over the past few years but these are the ones that I'm most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
       >
         <ul
           role="list"
