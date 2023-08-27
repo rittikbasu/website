6d3c1b17dcf4e3cb6857f286eff8d9eea5af5ab3
rittikbasu <irittik@gmail.com>
2023-08-28 01:10:01 +0530

    feat: update content + mockups

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,21 @@
-import memeGenerator from '@/images/projects/memeGenerator.png'
 import summariser from '@/images/projects/summariser.png'
 import trackrBot from '@/images/projects/trackrBot.png'
 import amicon from '@/images/projects/amicon.png'
 import yc from '@/images/projects/yc.png'
+import frize from '@/images/projects/frize.png'
 
 const data = [
   {
-    title: 'Price Alert Bot  (27⭐ on GitHub)',
+    title: 'Frize',
+    description:
+      'An interactive dashboard that transforms time-tracking data from Rize into beautiful charts, graphs and insights.',
+    techUsed: ['Next.js', 'Tailwind', 'Tremor UI', 'Supabase', 'OpenAI'],
+    image: frize,
+    link: 'https://frize.rittik.io',
+    github: 'https://github.com/rittikbasu/frize',
+  },
+  {
+    title: 'Price Alert Bot  (28 ⭐ on GitHub)',
     description:
       'A Telegram chatbot that helps you set price alerts for amazon products and sends you an alert message when it reaches the target price.',
     techUsed: ['Python', 'Telegram Bot API', 'ScraperAPI', 'Google Sheets API'],
@@ -24,6 +33,15 @@ const data = [
     github: 'https://github.com/rittikbasu/yc-job-client',
   },
   {
+    title: 'Amicon',
+    description:
+      'An intra campus networking web app for students of Amity University, Mumbai to connect with each other, see where their peers are from and find and share information more conveniently.',
+    techUsed: ['Javascript', 'Firebase', 'Bootstrap'],
+    image: amicon,
+    link: 'https://amicon-v1.web.app/',
+    github: '',
+  },
+  {
     title: 'Text Summarizer',
     description:
       'Summarises text content from a web article or user input and provides an elegant summary of the provided text using extractive summarization.',
@@ -36,24 +54,6 @@ const data = [
     ],
     link: 'https://summariser.rittikbasu.repl.co/',
     github: 'https://github.com/rittikbasu/text-summarizer',
-  },
-  {
-    title: 'Amicon',
-    description:
-      'An intra campus networking web app for students of Amity University, Mumbai to connect with each other and share information more conveniently.',
-    techUsed: ['Javascript', 'Firebase', 'Bootstrap'],
-    image: amicon,
-    link: 'https://amicon-v1.web.app/',
-    github: '',
-  },
-  {
-    title: 'Meme Generator',
-    description:
-      'A simple meme generator that lets you choose a template from the 100 most popular memes from Imgflip and create your own meme by adding text to it.',
-    techUsed: ['React', 'Bootstrap', 'Python', 'ImgflipAPI'],
-    image: memeGenerator,
-    link: 'https://rittikbasu.github.io/meme-generator',
-    github: 'https://github.com/rittikbasu/meme-generator',
   },
 ]
 
