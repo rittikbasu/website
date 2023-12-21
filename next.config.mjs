c1205b3d1b4dc4be1b081fc05c41786fbf7b73f7
rittikbasu <irittik@gmail.com>
2023-12-21 21:27:08 +0530

    chore: bump Next.js version

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    newNextLinkBehavior: true,
     scrollRestoration: true,
   },
   transpilePackages: ['react-tweet'],
@@ -12,12 +11,10 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'pbs.twimg.com' },
       { protocol: 'https', hostname: 'abs.twimg.com' },
-    ],
-    domains: [
-      'ik.imagekit.io',
-      's3.us-west-2.amazonaws.com',
-      'www.notion.so',
-      'images.unsplash.com',
+      { protocol: 'https', hostname: 'ik.imagekit.io' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 's3.us-west-2.amazonaws.com' },
+      { protocol: 'https', hostname: 'www.notion.so' },
     ],
   },
 }
